7b90081ce02b12943e8a35c5cfc8a661d7305fde
Jose Esteves <jose@desouza.work>
2022-06-15 13:06:18 +0200

    [JAKARTA] WIP

---
+++ src/main/java/com/accenture/j2ee/JaxrsActivator.java
@@ -0,0 +1,8 @@
+package com.accenture.j2ee;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class JaxrsActivator extends Application {
+}
---
+++ src/main/java/com/accenture/j2ee/api/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.accenture.j2ee.api;
+
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+
+@Path("hello")
+@RequestScoped
+public class HelloWorld {
+
+	@GET
+	public String hello() {
+		return "Heloo Jakarta EE";
+	}
+
+}
