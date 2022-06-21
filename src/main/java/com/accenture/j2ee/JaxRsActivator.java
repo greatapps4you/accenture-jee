939032510eb815da68d9c364dc38469072115b59
Jose Esteves <jose@desouza.work>
2022-06-21 16:37:03 +0200

    [jsf-2.3] REST:WIP

---
+++ src/main/java/com/accenture/j2ee/JaxRsActivator.java
@@ -0,0 +1,9 @@
+package com.accenture.j2ee;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class JaxRsActivator extends Application {
+
+}
--- src/main/java/com/accenture/j2ee/api/CustomerResource.java
+++ src/main/java/com/accenture/j2ee/api/CustomerResource.java
@@ -1,14 +1,13 @@
 package com.accenture.j2ee.api;
 
-import javax.enterprise.context.RequestScoped;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
 @Path("customers")
-@RequestScoped
 public class CustomerResource {
-	
+
 	@GET
+	@Path("list")
 	public String list() {
 		return "A LIST OF CUSTOMERS";
 	}
