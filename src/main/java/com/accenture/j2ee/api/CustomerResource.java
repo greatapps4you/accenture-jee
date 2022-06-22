535eb3f3b0021c45b16ed488a95c279dd09eb203
Jose Esteves <jose@desouza.work>
2022-06-22 19:03:42 +0200

    [jsf-2.3] REST

--- src/main/java/com/accenture/j2ee/api/CustomerResource.java
+++ src/main/java/com/accenture/j2ee/api/CustomerResource.java
@@ -1,15 +1,26 @@
 package com.accenture.j2ee.api;
 
+import java.util.List;
+
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.accenture.j2ee.entities.Customer;
+import com.accenture.j2ee.service.CustomerService;
 
 @Path("customers")
 public class CustomerResource {
+	
+	@Inject
+	private CustomerService customerService;
 
 	@GET
-	@Path("list")
-	public String list() {
-		return "A LIST OF CUSTOMERS";
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Customer> list() {
+		return customerService.list();
 	}
 
 }
